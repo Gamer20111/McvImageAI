459fdba3f3e2502a1377e697e6c747925296e9ca
Uczeń ZSŁ <piratPL620@gmail.com>
2023-04-17 16:14:22 +0200

    [W KILKU SLOWACH CO DODALISCIE]

--- MvcImageAI/Controllers/ImageObjectDetectionController.cs
+++ MvcImageAI/Controllers/ImageObjectDetectionController.cs
@@ -1,12 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using System.Text.Json;
+
 
 namespace MvcImageAI.Controllers
 {
+
     public class ImageObjectDetectionController : Controller
     {
+
+        private readonly string ApiUrl = "https://technikum01.cognitiveservices.azure.com";
+        private readonly string ApiKey = "d0b1d44871564786934befe406045484";
+        
         public IActionResult Index()
         {
             return View();
+            
         }
+        [HttpPost]
+        public async Task<IActionResult> Detect(IFormFile inputFile)
+        {
+
+            var apiClient = new ComputerVisionClient(new ApiKeyServiceClientCredentials(ApiKey)) { Endpoint = ApiUrl };
+            DetectResult detectResults = await apiClient.DetectObjectsInStreamAsync(inputFile.OpenReadStream());
+
+            ViewBag.Message = JsonSerializer.Serialize(detectResults);
+
+            return View("Index");
+        }
+        
     }
 }
