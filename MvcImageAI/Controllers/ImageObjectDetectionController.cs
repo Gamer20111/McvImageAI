46de1d9e389ef753373e366b7c531a573645b269
Uczeń ZSŁ <piratPL620@gmail.com>
2023-04-17 15:10:43 +0200

    First commit

---
+++ MvcImageAI/Controllers/ImageObjectDetectionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcImageAI.Controllers
+{
+    public class ImageObjectDetectionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcImageAI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcImageAI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
